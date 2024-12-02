7ff8edca486e235a34013811c9252202cfa81a6b
drwyspa <p.jedryczkowski@gmail.com>
2024-12-02 23:02:52 +0100

    Changes to be committed: 	modified:   libft/ft_split.c

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,152 +5,121 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/11 19:02:04 by pjedrycz          #+#    #+#             */
-/*   Updated: 2024/03/22 19:26:03 by pjedrycz         ###   ########.fr       */
+/*   Created: 2024/08/19 22:26:19 by pjedrycz          #+#    #+#             */
+/*   Updated: 2024/09/16 20:46:59 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
-// #include <string.h>
-// #include <fcntl.h>
-// #include <unistd.h>
-// #include <ctype.h>
-// #include <limits.h>
-#include <stdlib.h>
-#include "libft.h"
+#include "so_long.h"
 
-static void	ft_initiate_vars(size_t *i, int *j, int *s_word)
+static int	num_words_count(char const *s, char c);
+static void	split_words(char **result, char const *s, char c, int i);
+static void	empty_row_alert(char const *s, char c);
+
+/**
+ * @brief Splits a string by a delimiter into an array of words.
+ * 
+ * Allocates an array of strings where each element is a word from the input
+ * string 's', split by the delimiter 'c'. The array is null-terminated.
+ * 
+ * @param s The input string to split.
+ * @param c The delimiter character.
+ * @return A null-terminated array of words, or NULL on failure.
+ */
+char	**ft_split(char const *s, char c)
 {
-	*i = 0;
-	*j = 0;
-	*s_word = -1;
-}
-
-static int	ft_words(const char *str, char c)
-{
-	int	number;
-	int	flag;
-
-	number = 0;
-	flag = 0;
-	while (*str)
-	{
-		if (*str != c && flag == 0)
-		{
-			flag = 1;
-			number++;
-		}
-		else if (*str == c)
-			flag = 0;
-		str++;
-	}
-	return (number);
-}
-
-static char	*ft_wordcpy(const char *str, int start, int end)
-{
-	char	*word;
+	int		num_words;
+	char	**result;
 	int		i;
 
 	i = 0;
-	word = malloc((end - start +1) * sizeof(char));
-	if (!word)
+	empty_row_alert(s, c);
+	num_words = num_words_count(s, c);
+	result = (char **)malloc((num_words + 1) * sizeof(char *));
+	if (!result)
 		return (NULL);
-	while (start < end)
-	{
-		word[i] = str[start];
-		i++;
-		start++;
-	}
-	word[i] = '\0';
-	return (word);
+	split_words(result, s, c, i);
+	result[num_words] = NULL;
+	return (result);
 }
 
-static void	*ft_free(char **strs, int count)
+/**
+ * @brief Counts the number of words separated by a delimiter.
+ * 
+ * @param s The input string.
+ * @param c The delimiter character.
+ * @return The number of words.
+ */
+static int	num_words_count(char const *s, char c)
+{
+	int	num_words;
+	int	i;
+
+	num_words = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			num_words++;
+		i++;
+	}
+	return (num_words);
+}
+
+// Function to validate a map,
+// before it's even parsed.
+//
+static void	empty_row_alert(char const *s, char c)
 {
 	int	i;
 
 	i = 0;
-	while (i < count)
+	while (s[i] != '\0')
 	{
-		free(strs[i]);
-		i++;
-	}
-	free(strs);
-	return (NULL);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	char	**res;
-	size_t	i;
-	int		j;
-	int		s_word;
-
-	ft_initiate_vars(&i, &j, &s_word);
-	res = ft_calloc((ft_words(s, c) + 1), sizeof(char *));
-	if (!res)
-		return (NULL);
-	while (i <= ft_strlen(s))
-	{
-		if (s[i] != c && s_word < 0)
-			s_word = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		if (s[i] == c && s[i + 1] == c)
 		{
-			res[j] = ft_wordcpy(s, s_word, i);
-			if (!(res[j]))
-				return (ft_free(res, j));
-			s_word = -1;
-			j++;
+			write(2, ERR_EMPTY_ROW, 28);
+			write(2, "\n", 1);
+			exit(EXIT_FAILURE);
 		}
 		i++;
 	}
-	return (res);
 }
 
-// int	main()
-// {
-// 	char	**res;
-// 	int		i;
-// 	printf("String s : 'Hello world', separator c : ' ', result :\n");
-// 	res = ft_split("Hello world", ' ');
-// 	i = 0;
-// 	while (res[i])
-// 	{
-// 		printf("%s\n", res[i]);
-// 		i++;
-// 	}
-// 	printf("String s : ' Hello world ', separator c : ' ', result :\n");
-// 	res = ft_split(" Hello world ", ' ');
-// 	i = 0;
-// 	while (res[i])
-// 	{
-// 		printf("%s\n", res[i]);
-// 		i++;
-// 	}
-// 	printf("Strinh s : '   ', separator c : ' ', result :\n");
-// 	res = ft_split("   ", ' ');
-// 	i = 0;
-// 	while (res[i])
-// 	{
-// 		printf("%s\n", res[i]);
-// 		i++;
-// 	}
-// 	printf("String s : ' d  ', separator c : ' ', result :\n");
-// 	res = ft_split(" d  ", ' ');
-// 	i = 0;
-// 	while (res[i])
-// 	{
-// 		printf("%s\n", res[i]);
-// 		i++;
-// 	}
-// 	printf("String s : 'nosep', separator c : ' ', result :\n");
-// 	res = ft_split("nosep", ' ');
-// 	i = 0;
-// 	while (res[i])
-// 	{
-// 		printf("%s\n", res[i]);
-// 		i++;
-// 	}
-// 	return (0);
-// }
+/**
+ * @brief Splits the string into words and stores them in an array.
+ * 
+ * Allocates memory for each word and copies the word from the input string
+ * to the newly allocated memory. Each word is then stored in the result array.
+ * 
+ * @param result The array to store the words.
+ * @param s The input string.
+ * @param c The delimiter character.
+ * @param i The starting index for splitting.
+ */
+static void	split_words(char **result, char const *s, char c, int i)
+{
+	int	start;
+	int	j;
+	int	k;
+
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			result[j] = malloc((i - start + 1) * sizeof(char));
+			if (!result[j])
+				return ;
+			k = 0;
+			while (start < i)
+				result[j][k++] = s[start++];
+			result[j++][k] = '\0';
+		}
+		else
+			i++;
+	}
+}
